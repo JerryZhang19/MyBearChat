0f7c2da0a4c0d1456ce94bbd339393df0f098dfc
Jianwei <jwzhang19@gmail.com>
2021-02-18 15:50:47 +0800

    fix CORS path error in friends main.go

--- friends/main.go
+++ friends/main.go
@@ -30,7 +30,7 @@ func CORS(next http.Handler) http.Handler {
 
 		// Set headers
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Allow-Origin", "http://http://ec2-3-14-81-168.us-east-2.compute.amazonaws.com:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "http://ec2-3-14-81-168.us-east-2.compute.amazonaws.com:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
